c313c875bf233adde3d429fcdc8c8e05ec3e4935
Daniel <daniel.figg@gmail.com>
2024-01-23 12:19:35 -0500

    basic layout

--- main.js
+++ main.js
@@ -2,6 +2,19 @@ import './style.css';
 
 document.querySelector('#app').innerHTML = `
   <div>
-      HELLO
+      <h1>NaDa</h1>
+      <input placeholder='SEARCH' />
+      <ul class='image-container'>
+          <li class='image'>IMAGE 1</li>
+          <li class='image'>IMAGE 2</li>
+          <li class='image'>IMAGE 3</li>
+          <li class='image'>IMAGE 4</li>
+          <li class='image'>IMAGE 5</li>
+          <li class='image'>IMAGE 6</li>
+          <li class='image'>IMAGE 7</li>
+          <li class='image'>IMAGE 8</li>
+          <li class='image'>IMAGE 9</li>
+          <li class='image'>IMAGE 10</li>
+      </ul>
   </div>
 `;
